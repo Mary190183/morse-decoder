6669cc4ce1d68b0963b0971195a4fd2e3ef93a60
Mary190183 <pma190183@yandex.ru>
2022-07-15 21:38:56 +0300

    init: install

--- src/index.js
+++ src/index.js
@@ -38,7 +38,79 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    let answer = '';
+    
+
+
+    function getDigitalChar(array) {
+      let temp = [];
+      let decimal = '';
+      let digitalArray = [];
+      
+      for (let i = 0; i < 10; i++) {
+        temp.push(array[i]);
+      }
+
+      if (temp[0] == '*') {
+        return ['**********'];
+      }
+      
+      while (temp[0] === '0') {
+        temp.shift();
+      }
+
+      for (let k = 0; k < temp.length; k++) {
+        decimal+= temp[k];
+        if (k % 2 === 1) {
+          digitalArray.push(decimal);
+          decimal = '';
+        }
+      }
+      
+      return digitalArray;
+    } 
+
+   function removeDigitalChar(array) {
+     for (let i = 0; i < 10; i++) {
+       array.shift();
+     }
+     return array;
+   }
+
+  
+    
+    function getCharFromDigital (array) {
+      let answer = '';
+      let morse = '';
+      
+      if (array[0] === '**********') {
+        return ' ';
+      }
+
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] === '10') {
+          morse += '.';
+        } else {
+          morse += '-'
+        };
+      }
+
+        for (let key in MORSE_TABLE) {
+         if (morse === key) {
+           answer += MORSE_TABLE[key];
+         } 
+        }
+      return answer;
+    }
+
+    while(expr.length > 0) {
+      answer += getCharFromDigital(getDigitalChar(expr));
+      removeDigitalChar(expr);
+    }
+    return answer;
+
+ // write your solution here
 }
 
 module.exports = {
